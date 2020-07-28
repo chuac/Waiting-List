940042142363976a09079273764b234a41ef34bf
Christopher Chua <chuachristopher@hotmail.com>
2020-07-29 00:27:45 +0800

    Added Text-to-Speech functionality when double clicking a number to call out. Web Speech API

---
+++ src/components/TextToSpeech.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['message'],
+    methods: {
+        speak() {
+            let utterance = new SpeechSynthesisUtterance(this.message);
+            utterance.lang = 'en-AU';
+            speechSynthesis.speak(utterance);
+            //speechSynthesis.speak(new SpeechSynthesisUtterance(this.message));
+        }
+    },
+    mounted() {
+        this.speak();
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
