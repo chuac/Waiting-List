d8e96b6a6cb7cd8b2730738bd01b4db31c09f7e4
Christopher Chua <chuachristopher@hotmail.com>
2020-12-09 21:22:26 +0800

    Add a small pause between repeated TTS

--- src/components/TextToSpeech.vue
+++ src/components/TextToSpeech.vue
@@ -8,19 +8,25 @@
 export default {
     props: ['message'],
     methods: {
-        speak() {
+        async speak() {
             let utterance = new SpeechSynthesisUtterance(this.message);
             utterance.lang = 'en-GB';
             utterance.rate = 1; // set the speed, accepts between [0.1 - 10], defaults to 1
             utterance.pitch = 1; // set the pitch, accepts between [0 - 2], defaults to 1
             utterance.volume = 1; // set the volume, accepts between [0 - 1], defaults to 1
             speechSynthesis.cancel(); // seems to fix intermittent Chrome bug where speak() would randomly stop working https://stackoverflow.com/a/58775876/3024923
+            
             speechSynthesis.speak(utterance);
+            await this.sleep(3500); // wait 3500ms before continuing
             speechSynthesis.speak(utterance); // TTS now speaks twice!
+
             this.finishedSpeaking(); // emit custom event when speechSynthesis is done
         },
         finishedSpeaking: function() {
             this.$emit('finished-speaking');
+        },
+        sleep(ms) {
+            return new Promise((resolve) => setTimeout(resolve, ms));
         }
     },
     mounted() {
